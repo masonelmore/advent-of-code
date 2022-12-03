97fd77c925a7637285365429ff592a3cb2eb4eb3
Mason Elmore <masonelmore@users.noreply.github.com>
2022-12-03 17:58:10 -0600

    Puzzle -> part

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
     parser = argparse.ArgumentParser(description='Runner for AoC solutions.')
     parser.add_argument('-y', '--year', dest='year', type=str)
     parser.add_argument('-d', '--day', dest='day', type=str, action=PaddingAction)
-    parser.add_argument('-p', '--puzzle', dest='puzzle', type=str, action=PaddingAction)
+    parser.add_argument('-p', '--part', dest='part', type=str, action=PaddingAction)
     args = parser.parse_args()
 
-    solve = get_solve_func(args.year, args.day, args.puzzle)
+    solve = get_solve_func(args.year, args.day, args.part)
     input_filename = f'{args.year}/d{args.day}_input.txt'
     input_data = load_input(input_filename)
     solution = solve(input_data)
-    print(f'{args.year}/d{args.day}p{args.puzzle}: {solution}')
+    print(f'{args.year}/d{args.day}p{args.part}: {solution}')
 
 
 class PaddingAction(argparse.Action):
@@ -24,14 +24,14 @@ class PaddingAction(argparse.Action):
         setattr(namespace, self.dest, padded)
 
 
-def get_solve_func(year, day, puzzle):
-    filename = find_solution_file(year, day, puzzle)
+def get_solve_func(year, day, part):
+    filename = find_solution_file(year, day, part)
     module = importlib.import_module(f'{year}.{filename}')
     return module.solve
 
 
-def find_solution_file(year, day, puzzle):
-    file_pattern = f'{year}/d{day}p{puzzle}_*'
+def find_solution_file(year, day, part):
+    file_pattern = f'{year}/d{day}p{part}_*'
     files = list(Path('.').glob(file_pattern))
     if len(files) > 1:
         raise Exception(f'too many files found for "{file_pattern}"', files)
