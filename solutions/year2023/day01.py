e8cae9ccd387fe35a7d507cd3b36411d51ed7910
Mason Elmore <masonelmore@users.noreply.github.com>
2023-12-07 17:10:28 -0600

    Add 2023/day01/part2

--- solutions/year2023/day01.py
+++ solutions/year2023/day01.py
@@ -3,13 +3,13 @@ def solve_part1(data):
 
 
 def solve_part2(data):
-    return -1
+    return sum(calibration_values(data, parse_words=True))
 
 
 def calibration_values(data, parse_words=False):
     for line in data:
-        first_digit = find_digit(line)
-        last_digit = find_digit(line[::-1])
+        first_digit = parse_first_digit(line, parse_words)
+        last_digit = parse_last_digit(line, parse_words)
         yield int(first_digit + last_digit)
 
 
@@ -17,3 +17,40 @@ def find_digit(string):
     for char in string:
         if char.isdigit():
             return char
+
+
+def parse_first_digit(string, parse_words=False):
+    for i, char in enumerate(string):
+        if char.isdigit():
+            return char
+
+        if not parse_words:
+            continue
+
+        digit = parse_word(string[i:])
+        if digit:
+            return digit
+
+
+def parse_last_digit(string, parse_words=False):
+    for i, char in enumerate(string[::-1]):
+        if char.isdigit():
+            return char
+
+        if not parse_words:
+            continue
+
+        substring = string[len(string)-i-1:]
+        digit = parse_word(substring)
+        if digit:
+            return digit
+
+
+def parse_word(string):
+    words = [
+        'zero', 'one', 'two', 'three', 'four',
+        'five', 'six', 'seven', 'eight', 'nine'
+    ]
+    for digit, word in enumerate(words):
+        if string.startswith(word):
+            return str(digit)
--- tests/year2023/test_day01.py
+++ tests/year2023/test_day01.py
@@ -4,12 +4,24 @@ from solutions.year2023.day01 import solve_part1, solve_part2
 from tests.util import load_input, gen
 
 
-def example_input():
+def example_input1():
     return gen([
         '1abc2',
         'pqr3stu8vwx',
         'a1b2c3d4e5f',
         'treb7uchet',
+    ])
+
+
+def example_input2():
+    return gen([
+        'two1nine',
+        'eightwothree',
+        'abcone2threexyz',
+        'xtwone3four',
+        '4nineeightseven2',
+        'zoneight234',
+        '7pqrstsixteen',
     ])
 
 
@@ -24,18 +36,18 @@ class TestSolution(unittest.TestCase):
 
     def test_part2_full(self):
         data = load_input(self.input_filename)
-        expected = 0
+        expected = 53868
         answer = solve_part2(data)
         self.assertEqual(expected, answer)
 
     def test_part1_example(self):
-        data = example_input()
+        data = example_input1()
         expected = 142
         answer = solve_part1(data)
         self.assertEqual(expected, answer)
 
     def test_part2_example(self):
-        data = example_input()
-        expected = 0
+        data = example_input2()
+        expected = 281
         answer = solve_part2(data)
         self.assertEqual(expected, answer)
